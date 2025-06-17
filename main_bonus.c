648ea2993cd188dbd3826fafcd619f2e77c2f1b0
Ilan Nowak--Glandier <inowak--@2A5.42angouleme.fr>
2025-06-17 12:02:12 +0200

    [fix]: maj

--- main_bonus.c
+++ main_bonus.c
@@ -16,27 +16,54 @@ int     ft_list_size(t_list *begin_list);
 void	ft_list_remove_if(t_list **begin_list, void *data_ref, int (*cmp)(), void (*free_fct)(void *));
 void	ft_list_sort(t_list **begin_list, int (*cmp)());
 
+int cmp(void *data, void *data_ref)
+{
+    return (*(int *)data != *(int *)data_ref);
+}
+
+void free_fct(void *data)
+{
+    free(data);
+}
+
 int main(void) {
     t_list *list = NULL;
 
     int *a = malloc(sizeof(int));
+    int *a1 = malloc(sizeof(int));
+    int *a2 = malloc(sizeof(int));
+    int *a3 = malloc(sizeof(int));
     int *b = malloc(sizeof(int));
     int *c = malloc(sizeof(int));
     *a = 10;
+    *a1 = 10;
+    *a2 = 10;
+    *a3 = 10;
     *b = 20;
     *c = 30;
-
+    
     ft_list_push_front(&list, a);
+    ft_list_push_front(&list, a1);
+    ft_list_push_front(&list, a2);
+    ft_list_push_front(&list, a3);
     ft_list_push_front(&list, b);
     ft_list_push_front(&list, c);
 
+    t_list *tmp = list;
+    while (tmp)
+    {
+        printf("node: %p, data ptr: %p, value: %d\n", tmp, tmp->data, *(int *)(tmp->data));
+        tmp = tmp->next;
+    }
+
     printf("size: %d\n", ft_list_size(list));
 
-    t_list *tmp = list;
-    printf("content :\n");
-    while (tmp != NULL)
+    // ft_list_remove_if(&list, a, cmp, free_fct);
+
+    tmp = list;
+    while (tmp)
     {
-        printf("- %d\n", *(int *)(tmp->data));
+        printf("node: %p, data ptr: %p, value: %d\n", tmp, tmp->data, *(int *)(tmp->data));
         tmp = tmp->next;
     }
 
